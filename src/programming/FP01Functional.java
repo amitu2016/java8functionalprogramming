28b96c137b9d1ec23f080fb3e09c994abc6542cc
Amit Upadhyay <amitu@cdac.in>
2023-12-29 16:25:06 +0530

    commit

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -3,20 +3,34 @@ package programming;
 import java.util.List;
 
 public class FP01Functional {
-	
+
 	public static void main(String[] args) {
-		printAllNumbersInListFunctional(List.of(12,9,13,4,6,3,4,12,15));
+		List<Integer> list = List.of(12, 9, 13, 4, 6, 3, 4, 12, 15);
+		//printAllNumbersInListFunctional(list);
+		printEvenNumbersInListFunctional(list);
 	}
+
+	/*
+	 * public static void print(Integer number) { System.out.println(number); }
+	 */
 	
-	public static void print(Integer number) {
-		System.out.println(number);
+	public static boolean findEven(int num) {
+		return num % 2 == 0;
 	}
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-		
-		//Functional programming focuses on what to do
-		//Using method reference instead of lambda expression
-		numbers.stream().forEach(FP01Functional::print);
-		
+
+		// Functional programming focuses on what to do
+		// Using method reference instead of lambda expression
+		numbers.stream().forEach(System.out::println);
+
+	}
+
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+
+		numbers.stream()
+		.filter(FP01Functional::findEven) //method reference
+		.forEach(System.out::println);
+
 	}
 }
--- src/programming/FP01Structured.java
+++ src/programming/FP01Structured.java
@@ -3,17 +3,30 @@ package programming;
 import java.util.List;
 
 public class FP01Structured {
-	
+
 	public static void main(String[] args) {
-		printAllNumbersInListStructured(List.of(12,9,13,4,6,3,4,12,15));
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 3, 4, 12, 15);
+		// printAllNumbersInListStructured(numbers);
+		printEvenNumbersInListStructured(numbers);
 	}
 
 	private static void printAllNumbersInListStructured(List<Integer> numbers) {
-		
-		//Structured programming focuses on how to do
-		for(Integer num : numbers) {
+
+		// Structured programming focuses on how to do
+		for (Integer num : numbers) {
 			System.out.println(num);
 		}
-		
+
+	}
+
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+
+		// Structured programming focuses on how to do
+		for (Integer num : numbers) {
+			if (num % 2 == 0) {
+				System.out.println(num);
+			}
+		}
+
 	}
 }
